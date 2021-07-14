06a7c86f8a8a61f24ff9ecdd6920dfcb835499dd
Ivonne Sanchez <ivonnesoriano25@gmail.com>
2021-07-13 18:33:32 -0600

    changed loader

--- js/main.js
+++ js/main.js
@@ -1,48 +1,46 @@
 async function home() {
-    $('section').on('click', '.down', function() {
-        const height = $( window ).height();
+    $('section').on('click', '.down', function () {
+        const height = $(window).height();
         $('body, html').animate({
             scrollTop: height - 150
         }, 800);
     });
     const txt = ['Frontend Developer', 'Angular Developer']; /* The text */
-    let i =0;
-    while(true){
+    let i = 0;
+    while (true) {
         await typeSentence(txt[i], "#sentence");
         await waitForMs(1500);
         await deleteSentence("#sentence");
         await waitForMs(500);
         i++;
-        if(i >= txt.length)
-        {
-            i =0;
+        if (i >= txt.length) {
+            i = 0;
         }
     }
 
 
 }
 
-
-function header(){
-    $('.animated-button > span').each(function(index) {
+function header() {
+    $('.animated-button > span').each(function (index) {
         var characters = $(this).text().trim().split("");
         var label = $(this);
         label.empty();
-        $.each(characters, function(i, el) {
+        $.each(characters, function (i, el) {
             label.append("<em>" + el + "</em>");
         });
     });
 
-    $('header .top-menu').on('click', 'a', function() {
+    $('header .top-menu').on('click', 'a', function () {
         var link = $(this).attr('href');
         if (link.indexOf('#') == 0) {
-        
-                $('body, html').animate({
-                    scrollTop: $(link).offset().top - 115
-                }, 400);
-                if ($('header').hasClass('active')) {
-                    $('.menu-btn').trigger('click');
-                }
+
+            $('body, html').animate({
+                scrollTop: $(link).offset().top - 115
+            }, 400);
+            if ($('header').hasClass('active')) {
+                $('.menu-btn').trigger('click');
+            }
             // }
         } else {
             var preload = $('.preloader');
@@ -57,7 +55,7 @@ function header(){
             }, {
                 duration: 1000,
                 easing: [0.7, 0, 0.3, 1],
-                complete: function() {
+                complete: function () {
                     location.href = link;
                 }
             });
@@ -66,7 +64,7 @@ function header(){
     });
 
     if ($('section').length && $('.top-menu li a').length) {
-        $(window).on('scroll', function() {
+        $(window).on('scroll', function () {
             if (($(this).scrollTop() >= 5) && ($('section').length > 1)) {
                 $('.down').fadeOut();
             }
@@ -74,7 +72,7 @@ function header(){
                 $('.down').fadeIn();
             }
             var scrollPos = $(window).scrollTop();
-            $('.top-menu li a').each(function() {
+            $('.top-menu li a').each(function () {
                 if ($(this).attr('href').indexOf('#') == 0) {
                     var currLink = $(this);
                     var refElement = $(currLink.attr("href"));
@@ -89,16 +87,15 @@ function header(){
         });
     }
 
-    $(".hamburguer").click(function(){
+    $(".hamburguer").click(function () {
         $("body").toggleClass("fixed");
         $("header").toggleClass("open");
     })
 }
 
 
-
 function experience() {
-    const owl = $('.experience .owl-carousel'); 
+    const owl = $('.experience .owl-carousel');
     $('.owl-carousel').owlCarousel({
         margin: 40,
         autoplay: false,
@@ -123,16 +120,17 @@ function experience() {
 
     $('.experience .left').click(function () {
         owl.trigger('prev.owl.carousel');
-      });
-      $('.experience .right').click(function () {
+    });
+    $('.experience .right').click(function () {
         owl.trigger('next.owl.carousel');
-      });
-    
+    });
+
 }
 
+
 function education() {
-    const owl = $('.education .owl-carousel'); 
-   owl.owlCarousel({
+    const owl = $('.education .owl-carousel');
+    owl.owlCarousel({
         margin: 40,
         autoplay: false,
         autoplayTimeout: 5000,
@@ -156,45 +154,55 @@ function education() {
 
     $('.education .left').click(function () {
         owl.trigger('prev.owl.carousel');
-      });
-      $('.education .right').click(function () {
+    });
+    $('.education .right').click(function () {
         owl.trigger('next.owl.carousel');
-      });
-    
+    });
+
 }
+
 
 async function typeSentence(sentence, eleRef, delay = 100) {
     const letters = sentence.split("");
     let i = 0;
-    while(i < letters.length) {
-      await waitForMs(delay);
-      $(eleRef).append(letters[i]);
-      i++
+    while (i < letters.length) {
+        await waitForMs(delay);
+        $(eleRef).append(letters[i]);
+        i++
     }
     return;
-  }
-  
-  
-  function waitForMs(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
+}
 
-  async function deleteSentence(eleRef) {
+
+function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function deleteSentence(eleRef) {
     const sentence = $(eleRef).html();
     const letters = sentence.split("");
     let i = 0;
-    while(letters.length > 0) {
-      await waitForMs(100);
-      letters.pop();
-      $(eleRef).html(letters.join(""));
+    while (letters.length > 0) {
+        await waitForMs(100);
+        letters.pop();
+        $(eleRef).html(letters.join(""));
     }
-  }
+}
 
+function contact() {
+    window.onload = function () {
+        //hide the preloader
+        setTimeout(function () {
+            $('.preloader').slideUp();
+        }, 1000)
+    }
+}
 
-$(document).ready(function () {
-    header();
-    home();
-    experience();
-    education();
-    
-})
+    $(document).ready(function () {
+        header();
+        home();
+        experience();
+        education();
+        contact();
+
+    })
